d5f9ac92954d6885f62744df973234137d37c9bc
Earle Poole <eepoole4@gmail.com>
2019-10-07 17:02:27 -0500

    convert part3 db back to person

--- part3/mongo/mongo.js
+++ part3/mongo/mongo.js
@@ -1,32 +1,34 @@
 const mongoose = require('mongoose')
 
-if ( process.argv.length<3 ) {
+if( process.argv.length < 3){
   console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://fullstack:${password}@fullstackopen-kq5bx.mongodb.net/admin?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${}@fullstackopen-kq5bx.mongodb.net/admin?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(url, { useNewUrlParser: true})
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+  id: Number
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person =  mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
+const person = new Person({
+  content: 'HTML is easy',
   date: new Date(),
-  important: true,
+  important: true
 })
 
-note.save().then(response => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+person.save()
+  .then(res => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  .catch(err => console.log('could not save', err))
+
