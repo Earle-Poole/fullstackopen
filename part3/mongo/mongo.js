33ba611f2302603dcdf3b79d313aa3bcf3799460
Earle-Poole <eepoole4@gmail.com>
2019-10-08 07:50:30 -0500

    it works!

--- part3/mongo/mongo.js
+++ part3/mongo/mongo.js
@@ -1,34 +1,31 @@
 const mongoose = require('mongoose')
 
-if( process.argv.length < 3){
+if ( process.argv.length<3 ) {
   console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${}@fullstackopen-kq5bx.mongodb.net/admin?retryWrites=true&w=majority`
+const url = `mongodb://fullstack:${password}@fullstackopen-shard-00-00-kq5bx.mongodb.net:27017,fullstackopen-shard-00-01-kq5bx.mongodb.net:27017,fullstackopen-shard-00-02-kq5bx.mongodb.net:27017/test?ssl=true&replicaSet=FullstackOpen-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true})
+mongoose.connect(url, { useNewUrlParser: true })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
-  id: Number
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
-const Person =  mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  content: 'HTML is easy',
+  content: 'Earle Poole',
   date: new Date(),
-  important: true
+  important: true,
 })
 
-person.save()
-  .then(res => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
-  .catch(err => console.log('could not save', err))
-
+person.save().then(response => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
